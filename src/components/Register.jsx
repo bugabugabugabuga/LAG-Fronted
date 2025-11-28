a4ba6a292e7a31d19e96716de4b0bdf4da1a8d60
luka <lukakakhniashvili0@gmail.com>
2025-11-28 14:00:25 +0400

    Update Register.jsx

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -16,7 +16,7 @@ export default function Signup() {
 
     try {
       const resp = await fetch(
-        "http://localhost:3000/auth/sign-up",
+        "https://back-project-olive.vercel.app/auth/sign-up",
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
