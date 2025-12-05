eb4888ca082c9a73194429ac833b4c38a78c4a0d
bugabugabugabuga <Andria.sulkhanishvili@gmail.com>
2025-12-05 21:10:29 +0400

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ import "./Home.css";
 import { UserContext } from "../context/user-provider";
 
 const Home = () => {
-  console.log("VITE_SERVER_URL:", import.meta.env.VITE_SERVER_URL);
+console.log("SERVER URL:", import.meta.env.VITE_SERVER_URL);
 
 
   const navigate = useNavigate();
