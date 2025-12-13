4214d94ee848059fed393c15d5a6cbe74464b7ac
luka <lukakakhniashvili0@gmail.com>
2025-12-13 16:09:49 +0400

    fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -262,9 +262,9 @@ const Home = () => {
                 <p><strong>Author:</strong> {report.author?.fullname}</p>
 
                 {(userRole === "admin" || report.author?._id === userId) && (
-                  <button onClick={(e) => { e.stopPropagation(); setDeleteReportId(report._id); setShowDeleteModal(true); }}>Delete</button>
+                  <button className="mrg" onClick={(e) => { e.stopPropagation(); setDeleteReportId(report._id); setShowDeleteModal(true); }}>Delete</button>
                 )}
-                <button onClick={(e) => { e.stopPropagation(); handleReaction(report._id); }} style={{ display: "flex", alignItems: "center", gap: "6px" }}>
+                <button className="mrg" onClick={(e) => { e.stopPropagation(); handleReaction(report._id); }} style={{ display: "flex", alignItems: "center", gap: "6px" }}>
                   <ThumbsUp color={report.reactions?.likes?.includes(userId) ? "red" : "gray"} />
                   <span>{report.reactions?.likes?.length || 0}</span>
                 </button>
@@ -284,9 +284,9 @@ const Home = () => {
                 <p><strong>Author:</strong> {report.author?.fullname}</p>
 
                 {(userRole === "admin" || report.author?._id === userId) && (
-                  <button onClick={(e) => { e.stopPropagation(); setDeleteReportId(report._id); setShowDeleteModal(true); }}>Delete</button>
+                  <button className="mrg" onClick={(e) => { e.stopPropagation(); setDeleteReportId(report._id); setShowDeleteModal(true); }}>Delete</button>
                 )}
-                <button onClick={(e) => { e.stopPropagation(); handleReaction(report._id); }} style={{ display: "flex", alignItems: "center", gap: "6px" }}>
+                <button className="mrg" onClick={(e) => { e.stopPropagation(); handleReaction(report._id); }} style={{ display: "flex", alignItems: "center", gap: "6px" }}>
                   <ThumbsUp color={report.reactions?.likes?.includes(userId) ? "red" : "gray"} />
                   <span>{report.reactions?.likes?.length || 0}</span>
                 </button>
