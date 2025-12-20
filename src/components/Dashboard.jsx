9b8b880540df787bb8e85c6f3bea605bba77412e
luka <lukakakhniashvili0@gmail.com>
2025-12-20 13:03:03 +0400

    Update Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -205,8 +205,9 @@ const fetchPayments = async () => {
       const donorName = p.user?.fullname || "No donor";
       const donorEmail = p.user?.email || "N/A";
 
-const reportOwner = p.report?.user?.fullname || "No owner";
-const reportTitle = p.report?.title || "No title";
+const reportOwner = p.report?.author?.fullname || "No owner";
+const reportTitle = p.report?.description || "No title";
+
 
 
 
